bd03c033cc44cbf821e6f5ffae17b77a2f452c06
balaji09090 <balaji.jadhav.0900@gmail.com>
2026-05-31 00:17:37 +0530

    repo

--- MediCareManagement-main/src/main/java/com/example/demo/repository/RoomRepository.java
+++ MediCareManagement-main/src/main/java/com/example/demo/repository/RoomRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repository;
 
-public interface RoomRepository {
+	import java.util.Optional;
+	import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.stereotype.Repository;
+	import com.example.demo.model.Room;
+	@Repository
+public interface RoomRepository extends JpaRepository<Room, Integer>{
+	
+	    // This custom helper query searches your database for the first empty room matching a specific type
+	    Optional<Room> findFirstByRoomTypeAndIsAvailableTrue(String roomType);
+	}
 
-}
